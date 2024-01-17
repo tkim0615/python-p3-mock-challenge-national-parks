8569ac5154a2ddfe97803cd578264887df974dce
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-17 13:12:38 -0500

    prep

--- lib/testing/national_park_test.py
+++ lib/testing/national_park_test.py
@@ -124,12 +124,12 @@ class TestNationalParks:
         assert p_1.best_visitor().name == "Tom"
 
 
-#     def test_most_visited(self):
-#         """returns the most visited park"""
-#         p_1 = NationalPark("Yosemite")
-#         p_2 = NationalPark("Yellow Stone")
-#         vis_1 = Visitor("Tom")
-#         Trip(vis_1, p_1, "May 5th", "May 9th")
-#         Trip(vis_1, p_1, "January 5th", "January 20th")
-#         Trip(vis_1, p_2, "January 25th", "January 30th")
-#         assert NationalPark.most_visited().name == "Yosemite"
+    def test_most_visited(self):
+        """returns the most visited park"""
+        p_1 = NationalPark("Yosemite")
+        p_2 = NationalPark("Yellow Stone")
+        vis_1 = Visitor("Tom")
+        Trip(vis_1, p_1, "May 5th", "May 9th")
+        Trip(vis_1, p_1, "January 5th", "January 20th")
+        Trip(vis_1, p_2, "January 25th", "January 30th")
+        assert NationalPark.most_visited().name == "Yosemite"
