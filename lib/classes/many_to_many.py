d8032f0e7c3fdf945d98bc249886bb9be659b766
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-18 17:49:51 -0500

    prep

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,7 +2,14 @@ class NationalPark:
 
     def __init__(self, name):
         self.name = name
-        
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) >= 3 and not hasattr(self, 'name'):
+            self._name = name        
     def trips(self):
         pass
     
@@ -24,11 +31,28 @@ class Trip:
         self.start_date = start_date
         self.end_date = end_date
 
+    @property
+    def start_date(self):
+        return self._start_date
+    @start_date.setter
+    def start_date(self, start_date):
+        if isinstance(start_date, str) and len(start_date) >= 7 and (start_date.endswith('st') or start_date.endswith('nd') or start_date.endswith('rd') or start_date.endswith('th')):
+            self._start_date = start_date
+
 
 class Visitor:
 
     def __init__(self, name):
         self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1<= len(name) <=15:
+            self._name = name
+    
         
     def trips(self):
         pass
--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,19 @@ from classes.many_to_many import Trip
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+    v1= Visitor('tyler')
+    v2= Visitor('max')
+    v3= Visitor('harry')
+    v4= Visitor('ronaldo')
+
+    p1= NationalPark('grand canyon')
+    p2= NationalPark('mansuk')
+    p3= NationalPark('CENTRAL')
+    p4= NationalPark('dulles')
+
+    t1 = Trip(v1, p1, 'jan 1st', 'jan 4th')
+    t2 = Trip(v2, p1, 'jan 1st', 'jan 4th')
+    t3 = Trip(v2, p1, 'jan 1st', 'jan 4th')
+    t4 = Trip(v1, p1, 'jan 1st', 'jan 4th')
+
     ipdb.set_trace()
